026d75114b4b538e6d919b1a927edde449e4fcda
samarthsingla <25768584+samarthsingla@users.noreply.github.com>
2023-01-11 23:53:28 +0530

    Routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Header from "./components/header/Header";
 import Hero from "./pages/homepage/hero/Hero";
 import Home from "./pages/homepage/Home";
 import WinterSchool from "./pages/winterschool/WinterSchool";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 export default class App extends React.Component{
     render(){
         return(
-            <HashRouter>
+            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/winterschool" element={<WinterSchool/>}/>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         );
     }
 }
